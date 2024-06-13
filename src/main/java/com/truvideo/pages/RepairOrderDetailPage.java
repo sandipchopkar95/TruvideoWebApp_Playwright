792698bbf305f3f0a73020ced449693dd18104c6
Sandip Chopkar <sandipchopkar789@gmail.com>
2024-06-13 16:29:14 +0530

    new

--- src/main/java/com/truvideo/pages/RepairOrderDetailPage.java
+++ src/main/java/com/truvideo/pages/RepairOrderDetailPage.java
@@ -56,7 +56,7 @@ public class RepairOrderDetailPage extends JavaUtility {
 		return !flags.contains(false);
 	}
 
-	private String addVideo_Title = "div.video-library__title";
+	private String addVideo_Title = "div.video-library__title p";
 	private String operations_Buttons = ".menu-options";
 
 	private String sendToCustomer_Button = "button span:has-text('Send to customer')";
@@ -76,27 +76,31 @@ public class RepairOrderDetailPage extends JavaUtility {
 		return className;
 	}
 
+	private String videos = "img[alt='video thumbnail']";
+	private String add_Button = "div.video-library__add-video-container button";
+	private String added_Video = "div.orders-detail-menu__media-videos img";
+
 	public void sendVideoToCustomer() {
 		FrameLocator frame = page.frameLocator(orderDetailsIFrame);
 		page.waitForURL(url -> url.contains("order/service/view"));
 		List<Boolean> flags = new ArrayList<Boolean>();
 		SoftAssert softAssert = new SoftAssert();
-		String sendToCustomerClass=selectButton("Send to customer");
-		String viewWithCustomerClass=selectButton("View with customer");
+		String sendToCustomerClass = selectButton("Send to customer");
+		String viewWithCustomerClass = selectButton("View with customer");
 		if (sendToCustomerClass == null || viewWithCustomerClass == null) {
-	        logger.info("'Send to customer' or 'View with customer' button is not found");
-	        flags.add(false);
-	    } else if (sendToCustomerClass.contains("disabled") && viewWithCustomerClass.contains("disabled")) {
-	        logger.info("Both 'Send to customer' & 'View with customer' button is disabled");
-	        flags.add(true);
-	    } else {
-	        logger.info("'Send to customer' or 'View with customer' button is not disabled");
-	        flags.add(false);
-	    }
+			logger.info("'Send to customer' or 'View with customer' button is not found");
+			flags.add(false);
+		} else if (sendToCustomerClass.contains("disabled") && viewWithCustomerClass.contains("disabled")) {
+			logger.info("Both 'Send to customer' & 'View with customer' button is disabled");
+			flags.add(true);
+		} else {
+			logger.info("'Send to customer' or 'View with customer' button is not disabled");
+			flags.add(false);
+		}
 		softAssert.assertTrue(!flags.contains(false),
 				"Verify 'Send to customer' & 'View with customer' button is disabled");
 		flags.clear();
-		frame.locator(addMedia).click();	
+		frame.locator(addMedia).click();
 		if (frame.locator(addVideo_Title).textContent().equals("Add video")) {
 			logger.info("Multimedia Screen opened: " + frame.locator(addVideo_Title).textContent());
 			flags.add(true);
@@ -105,6 +109,33 @@ public class RepairOrderDetailPage extends JavaUtility {
 			flags.add(false);
 		}
 		softAssert.assertTrue(!flags.contains(false), "Verify Add Media button is clickable");
+		flags.clear();
+		frame.locator(videos).first().click();
+		page.waitForTimeout(2000);
+		frame.locator(add_Button).click();
+		page.waitForTimeout(4000);
+		String sendToCustomerClass_AfterVideoAdded = selectButton("Send to customer");
+		String viewWithCustomerClass_AfterVideoAdded = selectButton("View with customer");
+		int addedVideoCount = frame.locator(added_Video).count();
+		if (addedVideoCount <= 0) {
+			logger.info("Video added sucessfull and visible on media gallery");
+			flags.add(true);
+		}else {
+			logger.info("Selected Video not added to media gallery");
+			flags.add(true);
+		}
+		if (sendToCustomerClass_AfterVideoAdded == null || viewWithCustomerClass_AfterVideoAdded == null) {
+			logger.info("'Send to customer' or 'View with customer' button is not found");
+			flags.add(false);
+		} else if (!sendToCustomerClass_AfterVideoAdded.contains("disabled")
+				&& !viewWithCustomerClass_AfterVideoAdded.contains("disabled")) {
+			logger.info("Both 'Send to customer' & 'View with customer' button is enabled");
+			flags.add(true);
+		} else {
+			logger.info("'Send to customer' or 'View with customer' button is disabled");
+			flags.add(false);
+		}
+		softAssert.assertTrue(!flags.contains(false), "Verify add video function");
 		softAssert.assertAll();
 	}
 
